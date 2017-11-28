01eed3836bc4e7748a7e97f366153161ef4dd42a
ogrikristins <34062209+ogrikristins@users.noreply.github.com>
2017-11-28 17:56:36 +0000

    Update and rename project1bogri.cpp to project1b copy.cpp

--- project1b copy.cpp
+++ project1b copy.cpp
@@ -4,24 +4,31 @@
 using namespace std;
 
 int main() {
+
     int lines;
-    cout << "How many lines of text? " << endl;
-    cin >> lines;
+
+        cout << "How many lines of text? " << endl;
+        cin >> lines;
+
     string str;
+
     cout << "Enter text: " << endl;
+
     for(int i = 0; i < lines; i++){
+
     cin >> ws;
     getline(cin, str);
+
     ofstream fout;
+
     fout.open("project1b.txt", ios::app);
        if(str[0] == '\\') {
             return 0;
-    }
-    else{
-    fout << str << endl;
-    }
+        }
+        else{
+            fout << str << endl;
+        }
     }
     return 0;
-
 }
 
