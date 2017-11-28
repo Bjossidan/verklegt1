59a580cb2290b1a9292287c5b976a4cf5bacf4fc
ogrikristins <34062209+ogrikristins@users.noreply.github.com>
2017-11-28 17:59:40 +0000

    bidur um hversu margar linur, breyta thvi kannski?

--- project1b copy.cpp
+++ project1b copy.cpp
@@ -6,26 +6,22 @@ using namespace std;
 int main() {
 
     int lines;
-
         cout << "How many lines of text? " << endl;
         cin >> lines;
 
     string str;
-
     cout << "Enter text: " << endl;
-
     for(int i = 0; i < lines; i++){
 
     cin >> ws;
     getline(cin, str);
 
     ofstream fout;
-
     fout.open("project1b.txt", ios::app);
        if(str[0] == '\\') {
             return 0;
         }
-        else{
+        else {
             fout << str << endl;
         }
     }
