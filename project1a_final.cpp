b6789eafcc0bcc54d08fdc8730d91c7f860b1735
Bjossidan <34061953+Bjossidan@users.noreply.github.com>
2017-12-05 15:28:49 +0000

    Update project1a_final.cpp
    Changed from input to output

--- project1a_final.cpp
+++ project1a_final.cpp
@@ -4,27 +4,39 @@
 
 using namespace std;
 
+int main() {
+    string str;
+    ifstream fin;
+    int count = 0;
+    char ans;
 
-int main()
-{
-    string txt;
-    for(int i = 0; i<10; i++){
-        cin >> ws;
-        getline(cin, txt);
-        ofstream fout;
-        fout.open("project1a.txt", ios::app);
-        fout << txt << endl;
-        fout.close();
+    fin.open("text.txt");
+    do {
+        while(count < 10 && !fin.eof()) {
+
+            getline(fin, str);
+            cout << str << endl;
+            count ++;
         }
-    char choose;
-    cout << "Do you wish to continue (y/n)?" << endl;
-    cin >> choose;
-    if(choose == 'y'){
-        return main();
-    }
-    else if(choose == 'n'){
-        return 0;
-    }
+        if (fin.eof ()) {
+            return 0;
+        }
+        cout << "Do you wish to continue (y/n)? " << endl;
+        cin >> ans;
+        count = 0;
+        if(ans == 'n'|| ans == 'N') {
+            return 0;
+        }
+        if (!(ans == 'y'|| ans == 'Y')) {
+            cout << "Do you wish to continue (y/n)? " << endl;
+            cin >> ans;
+        }
+        if (fin.eof ()) {
+            return 0;
+        }
 
+    }while (ans == 'y'|| ans == 'Y');
+    fin.close();
 
+    return 0;
 }
